d33dcfaf7d49a3201f40b93d3a90b795b610b9e2
amakor97 <amakor.andrei@gmail.com>
2024-05-21 13:12:42 +0300

    [TODO-LIST] update: add tasks array

---
+++ src/tasks.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    id: 1, 
+    startDate: "2024.05.13", 
+    finishDate: "2024.08.05", 
+    comments: ["important", "not urgent"], 
+    description: "study js react course", 
+    participants: ["myself"],
+    status: "not finished"
+  },
+  {
+    id: 2, 
+    startDate: "2024.04.27", 
+    finishDate: "2024.05.13", 
+    comments: ["not important", "not urgent"], 
+    description: "enjoy vacation", 
+    participants: ["myself"],
+    status: "finished"
+  },
+  {
+    id: 3, 
+    startDate: "2024.05.20", 
+    finishDate: "2024.05.24", 
+    comments: ["important", "urgent"], 
+    description: "do some work tasks", 
+    participants: ["myself", "my colleguaes"],
+    status: "not finished"
+  },
+  {
+    id: 4, 
+    startDate: "2024.05.20", 
+    finishDate: "2024.05.20", 
+    comments: ["important", "immediate"], 
+    description: "go to see a doctor", 
+    participants: ["myself"],
+    status: "finished"
+  },
+  {
+    id: 5, 
+    startDate: "2024.01.01", 
+    finishDate: "2024.12.31", 
+    comments: ["important", "not urgent"], 
+    description: "improve web dev skills", 
+    participants: ["myself"],
+    status: "not finished"
+  }
+]
