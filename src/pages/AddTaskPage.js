96b61748a1896202107e574569330a17b34b71d8
amakor97 <amakor.andrei@gmail.com>
2024-07-15 22:35:41 +0300

    clean add and update task pages

--- src/pages/AddTaskPage.js
+++ src/pages/AddTaskPage.js
@@ -1,6 +1,9 @@
-import AddTaskForm from "../components/addTaskForm/AddTaskForm";
-import { getTasksId, publishTask } from "../requests/tasksRequests";
+
 import { redirect } from "react-router-dom";
+
+import AddTaskForm from "../components/addTaskForm/AddTaskForm";
+
+import { getTasksId, publishTask } from "../requests/tasksRequests";
 
 
 function getMaxId(ids) {
@@ -15,6 +18,7 @@ function getMaxId(ids) {
   return maxId;
 }
 
+
 export async function idsLoader() {
   let ids = await getTasksId();
   return {ids};
@@ -23,35 +27,30 @@ export async function idsLoader() {
 
 export async function createAction({request}){
   const data = Object.fromEntries(await request.formData());
-  console.log(data);
 
   data.id = undefined;
+  
   data.startDate = data.start.replaceAll("-", ".");
   data.finishDate = data.finish.replaceAll("-", ".");
+  delete data.start;
+  delete data.finish;
 
-
-  console.log(data.comments);
   data.comments = [data.comments1, data.comments2].filter(value => value);
   delete data.comments1;
   delete data.comments2;
-
-
-  delete data.start;
-  delete data.finish;
 
   data.participants = data.participants.split(", ");
   data.status = "not finished";
 
   const {ids} = await idsLoader();
-  console.log(ids);
   data.id = getMaxId(ids) + 1;
 
   await publishTask(data);
   return redirect("/");
 }
 
-export default function AddTaskPage() {
 
+export default function AddTaskPage() {
   return (
     <>
       <AddTaskForm/>
--- src/pages/UpdateTaskPage.js
+++ src/pages/UpdateTaskPage.js
@@ -1,23 +1,20 @@
-import { useLoaderData, useNavigate } from "react-router-dom";
+import { useLoaderData, redirect } from "react-router-dom";
+
 import AddTaskForm from "../components/addTaskForm/AddTaskForm";
+
 import { getTaskById, updateTask } from "../requests/tasksRequests";
-import { useContext } from "react";
+
 import { PageSettings } from "../pageSettings";
-import { redirect } from "react-router-dom";
+
 
 export async function taskByIdLoader({params}) {
-  console.log(params);
   const task = await getTaskById(params.taskId);
-  console.log(task);
   return {task};
 }
 
 
 export async function updateAction({request, params}) {
   const data = Object.fromEntries(await request.formData());
-
-  console.log("upd:");
-  console.log(data);
 
   data.id = +params.taskId;
 
@@ -27,24 +24,21 @@ export async function updateAction({request, params}) {
   
   data.startDate = data.start.replaceAll("-", ".");
   data.finishDate = data.finish.replaceAll("-", ".");
-
   delete data.start;
   delete data.finish;
 
   const defData = await getTaskById(data.id);
-  console.log(defData);
   data.description = defData.description;
   data.status = defData.status;
 
   data.participants = data.participants.split(", ");
-  const task = await updateTask(data, params.taskId);
+  await updateTask(data, params.taskId);
   return redirect("/");
 }
 
 
 export default function AddTaskPage() {
   const {task} = useLoaderData();
-  console.log(task);
 
   return (
     <PageSettings.Provider value={{task}}>
